b8ebe562e8365ae95d49cba009c56a5f834b1010
thongnq95 <quocthonght95@gmail.com>
2017-07-14 23:26:15 +0700

    static-admin

---
+++ resources/views/admin/partials/header.blade.php
@@ -0,0 +1,429 @@
+<div id="headerbar">
+	<div class="container-fluid">
+		<div class="row">
+			<div class="col-xs-6 col-sm-2">
+				<a href="#" class="shortcut-tiles tiles-brown">
+					<div class="tiles-body">
+						<div class="pull-left"><i class="fa fa-pencil"></i></div>
+					</div>
+					<div class="tiles-footer">
+						Create Post
+					</div>
+				</a>
+			</div>
+			<div class="col-xs-6 col-sm-2">
+				<a href="#" class="shortcut-tiles tiles-grape">
+					<div class="tiles-body">
+						<div class="pull-left"><i class="fa fa-group"></i></div>
+						<div class="pull-right"><span class="badge">2</span></div>
+					</div>
+					<div class="tiles-footer">
+						Contacts
+					</div>
+				</a>
+			</div>
+			<div class="col-xs-6 col-sm-2">
+				<a href="#" class="shortcut-tiles tiles-primary">
+					<div class="tiles-body">
+						<div class="pull-left"><i class="fa fa-envelope-o"></i></div>
+						<div class="pull-right"><span class="badge">10</span></div>
+					</div>
+					<div class="tiles-footer">
+						Messages
+					</div>
+				</a>
+			</div>
+			<div class="col-xs-6 col-sm-2">
+				<a href="#" class="shortcut-tiles tiles-inverse">
+					<div class="tiles-body">
+						<div class="pull-left"><i class="fa fa-camera"></i></div>
+						<div class="pull-right"><span class="badge">3</span></div>
+					</div>
+					<div class="tiles-footer">
+						Gallery
+					</div>
+				</a>
+			</div>
+
+			<div class="col-xs-6 col-sm-2">
+				<a href="#" class="shortcut-tiles tiles-midnightblue">
+					<div class="tiles-body">
+						<div class="pull-left"><i class="fa fa-cog"></i></div>
+					</div>
+					<div class="tiles-footer">
+						Settings
+					</div>
+				</a>
+			</div>
+			<div class="col-xs-6 col-sm-2">
+				<a href="#" class="shortcut-tiles tiles-orange">
+					<div class="tiles-body">
+						<div class="pull-left"><i class="fa fa-wrench"></i></div>
+					</div>
+					<div class="tiles-footer">
+						Plugins
+					</div>
+				</a>
+			</div>
+		</div>
+	</div>
+</div>
+<header id="topnav" class="navbar navbar-inverse navbar-fixed-top clearfix" role="banner">
+	<a id="leftmenu-trigger" class="" data-toggle="tooltip" data-placement="right" title="Toggle Sidebar"></a>
+	<a class="navbar-brand" href="index-2.html">Avalon</a>
+	<a id="rightmenu-trigger" class="" data-toggle="tooltip" data-placement="left" title="Toggle Infobar"></a>
+	<div class="yamm navbar-left navbar-collapse collapse">
+		<ul class="nav navbar-nav">
+			<li class="dropdown">
+				<a href="#" class="dropdown-toggle" data-toggle="dropdown"><i class="fa fa-random mr5"></i>Mega Menu<span class="caret"></span></a>
+				<ul class="dropdown-menu" style="width: 900px;">
+					<li>
+						<div class="yamm-content container-sm-height">
+							<div class="row row-sm-height yamm-col-bordered">
+								<div class="col-sm-3 col-sm-height yamm-col">
+
+									<h3 class="yamm-category">Sidebar</h3>
+									<ul class="list-unstyled mb20">
+										<li><a href="layout-fixed-sidebars.html">Stretch Sidebars</a></li>
+										<li><a href="layout-sidebar-scroll.html">Scroll Sidebar</a></li>
+										<li><a href="layout-static-leftbar.html">Static Sidebar</a></li>
+										<li><a href="layout-leftbar-widgets.html">Sidebar Widgets</a></li>   
+									</ul>
+
+									<h3 class="yamm-category">Infobar</h3>
+									<ul class="list-unstyled">
+										<li><a href="layout-infobar-offcanvas.html">Offcanvas Infobar</a></li>
+										<li><a href="layout-infobar-overlay.html">Overlay Infobar</a></li>
+										<li><a href="layout-chatbar-overlay.html">Chatbar</a></li>
+										<li><a href="layout-rightbar-widgets.html">Infobar Widgets</a></li>   
+									</ul>
+
+								</div>
+								<div class="col-sm-3 col-sm-height yamm-col">
+
+									<h3 class="yamm-category">Page Content</h3>
+									<ul class="list-unstyled mb20">
+										<li><a href="layout-breadcrumb-top.html">Breadcrumbs on Top</a></li>
+										<li><a href="layout-page-tabs.html">Page Tabs</a></li>
+										<li><a href="layout-fullheight-panel.html">Full-Height Panel</a></li>
+										<li><a href="layout-fullheight-content.html">Full-Height Content</a></li>
+									</ul>
+
+									<h3 class="yamm-category">Misc</h3>
+									<ul class="list-unstyled">
+										<li><a href="layout-topnav-options.html">Topnav Options</a></li>
+										<li><a href="layout-horizontal-small.html">Horizontal Small</a></li>
+										<li><a href="layout-horizontal-large.html">Horizontal Large</a></li>
+										<li><a href="layout-boxed.html">Boxed</a></li>
+									</ul>
+
+								</div>
+								<div class="col-sm-3 col-sm-height yamm-col">
+
+									<h3 class="yamm-category">Analytics</h3>
+									<ul class="list-unstyled mb20">
+										<li><a href="charts-flot.html">Flot</a></li>
+										<li><a href="charts-sparklines.html">Sparklines</a></li>
+										<li><a href="charts-morris.html">Morris</a></li>
+										<li><a href="charts-easypiechart.html">Easy Pie Charts</a></li>
+									</ul>
+
+									<h3 class="yamm-category">Components</h3>
+									<ul class="list-unstyled">
+										<li><a href="ui-tiles.html">Tiles</a></li>
+										<li><a href="custom-knob.html">jQuery Knob</a></li>
+										<li><a href="custom-jqueryui.html">jQuery Slider</a></li>
+										<li><a href="custom-ionrange.html">Ion Range Slider</a></li>
+									</ul>
+
+								</div>
+								<div class="col-sm-3 col-sm-height yamm-col">
+									<h3 class="yamm-category">Rem</h3>
+									<img src="assets/demo/stockphoto/communication_12_carousel.jpg" class="mb20 img-responsive" style="width: 100%;">
+									<p>Sed ut perspiciatis unde omnis iste natus error sit voluptatem accusantium doloremque laudantium. totam rem aperiam eaque ipsa quae ab illo inventore veritatis et quasi architecto beatae vitae dicta sunt explicabo.</p>
+								</div>
+							</div>
+						</div>
+					</li>
+				</ul>
+			</li>
+			<li class="dropdown" id="widget-classicmenu">
+				<a href="#" class="dropdown-toggle" data-toggle="dropdown">Dropdown<span class="caret"></span></a>
+				<ul class="dropdown-menu" role="menu">
+					<li><a href="#">Action</a></li>
+					<li><a href="#">Another action</a></li>
+					<li><a href="#">Something else here</a></li>
+					<li class="divider"></li>
+					<li><a href="#">Separated link</a></li>
+					<li class="divider"></li>
+					<li><a href="#">One more separated link</a></li>
+				</ul>
+			</li>
+			<li><a href="frontend/index.html" target="_blank">Frontend</a></li>
+			<li><a href="landing/index.html" target="_blank">Landing Page</a></li>
+		</ul>
+	</div>
+
+	<ul class="nav navbar-nav toolbar pull-right">
+		<li class="dropdown">
+			<a href="#" id="navbar-links-toggle" data-toggle="collapse" data-target="header>.navbar-collapse">&nbsp;</a>
+		</li>
+
+		<li class="toolbar-icon-bg demo-headerdrop-hidden">
+			<a href="#" id="headerbardropdown"><span class="icon-bg"><i class="fa fa-fw fa-level-down"></i></span></i></a>
+		</li>
+
+
+		<li class="dropdown toolbar-icon-bg hidden-xs">
+			<a href="#" class="hasnotifications dropdown-toggle" data-toggle='dropdown'><span class="icon-bg"><i class="fa fa-fw fa-tasks"></i></span><span class="badge badge-warning">7</span></a>
+			<div class="dropdown-menu taskprogresses arrow">
+				<div class="dd-header">
+					<span>Tasks</span>
+					<span><a href="#">Settings</a></span>
+				</div>
+				<ul class="scrollthis">
+					<li>
+						<a href="#">
+							<div class="contextual-progress">
+								<div class="clearfix">
+									<div class="progress-title">Template Styling</div>
+									<div class="progress-percentage">5/25</div>
+								</div>
+								<div class="progress progress-lg">
+									<div class="progress-bar progress-bar-inverse" style="width: 20%"></div>
+								</div>
+							</div>
+						</a>
+					</li>
+					<li>
+						<a href="#">
+							<div class="contextual-progress">
+								<div class="clearfix">
+									<div class="progress-title">App Development</div>
+									<div class="progress-percentage">3/9</div>
+								</div>
+								<div class="progress progress-lg progress-striped active">
+									<div class="progress-bar progress-bar-success" style="width: 33%"></div>
+								</div>
+							</div>
+						</a>
+					</li>
+					<li>
+						<a href="#">
+							<div class="contextual-progress">
+								<div class="clearfix">
+									<div class="progress-title">App Interface</div>
+									<div class="progress-percentage">15%</div>
+								</div>
+								<div class="progress">
+									<div class="progress-bar progress-bar-danger" style="width: 15%"></div>
+								</div>
+							</div>
+						</a>
+					</li>
+					<li>
+						<a href="#">
+							<div class="contextual-progress">
+								<div class="clearfix">
+									<div class="progress-title">Documentation</div>
+									<div class="progress-percentage">4/5</div>
+								</div>
+								<div class="progress">
+									<div class="progress-bar progress-bar-primary" style="width: 80%"></div>
+								</div>
+							</div>
+						</a>
+					</li>
+					<li>
+						<a href="#">
+							<div class="contextual-progress">
+								<div class="clearfix">
+									<div class="progress-title">eCommerce</div>
+									<div class="progress-percentage">6/10</div>
+								</div>
+								<div class="progress progress-striped active">
+									<div class="progress-bar progress-bar-warning" style="width: 60%"></div>
+								</div>
+							</div>
+						</a>
+					</li>
+
+
+				</ul>
+				<div class="dd-footer">
+					<a href="#">View all tasks</a>
+				</div>
+			</div>
+		</li>
+
+
+		<li class="dropdown toolbar-icon-bg">
+			<a href="#" class="hasnotifications dropdown-toggle" data-toggle='dropdown'><span class="icon-bg"><i class="fa fa-fw fa-bell"></i></span><span class="badge badge-alizarin">5</span></a>
+			<div class="dropdown-menu notifications arrow">
+				<div class="dd-header">
+					<span>Notifications</span>
+					<span><a href="#">Settings</a></span>
+				</div>
+				<ul class="scrollthis">
+					<li class="">
+						<a href="#" class="notification-success">
+							<div class="notification-icon"><i class="fa fa-check fa-fw"></i></div>
+							<div class="notification-content"><strong>Lorem ipsum</strong> dolor sit amet consectetur adipisicing elit!</div>
+							<div class="notification-time">40m</div>
+						</a>
+					</li>
+					<li class="">
+						<a href="#" class="notification-danger">
+							<div class="notification-icon"><i class="fa fa-times fa-fw"></i></div>
+							<div class="notification-content">Etiam porta sem malesuada</div>
+							<div class="notification-time">5h</div>
+						</a>
+					</li>
+					<li class="">
+						<a href="#" class="notification-inverse">
+							<div class="notification-icon"><i class="fa fa-cloud fa-fw"></i></div>
+							<div class="notification-content"><strong>Nesciunt</strong> reprehenderit provident distinctio eveniet cupiditate atque</div>
+							<div class="notification-time">16h</div>
+						</a>
+					</li>
+					<li class="">
+						<a href="#" class="notification-warning">
+							<div class="notification-icon"><i class="fa fa-warning fa-fw"></i></div>
+							<div class="notification-content">Aperiam accusamus modi ipsum officia quas nisi!</div>
+							<div class="notification-time">1d</div>
+						</a>
+					</li>
+					<li class="">
+						<a href="#" class="notification-info">
+							<div class="notification-icon"><i class="fa fa-shopping-cart fa-fw"></i></div>
+							<div class="notification-content">Libero distinctio eveniet</div>
+							<div class="notification-time">5d</div>
+						</a>
+					</li>
+					<li class="">
+						<a href="#" class="notification-primary">
+							<div class="notification-icon"><i class="fa fa-comment fa-fw"></i></div>
+							<div class="notification-content">Officiis modi ipsum officia ad dolor sit amet consectetur sit voluptatem accusantium doloremque laudantium totam rem aperiam</div>
+							<div class="notification-time">8d</div>
+						</a>
+					</li>
+				</ul>
+				<div class="dd-footer">
+					<a href="#">View all notifications</a>
+				</div>
+			</div>
+		</li>
+
+		<li class="dropdown toolbar-icon-bg hidden-xs">
+			<a href="#" class="hasnotifications dropdown-toggle" data-toggle='dropdown'><span class="icon-bg"><i class="fa fa-fw fa-envelope"></i></span><span class="badge badge-info">1</span></a>
+			<div class="dropdown-menu messages arrow">
+				<div class="dd-header">
+					<span>Messages</span>
+					<span><a href="#">Settings</a></span>
+				</div>
+
+				<ul class="scrollthis">
+					<li class="">
+						<a href="#">
+							<img class="msg-avatar" src="assets/demo/avatar/avatar_09.png" alt="avatar" />
+							<div class="msg-content">
+								<span class="name">Steven Shipe</span>
+								<span class="msg">Nonummy nibh epismod lorem ipsum</span>
+							</div>
+							<span class="msg-time">30s</span>
+						</a>
+					</li>
+					<li>
+						<a href="#">
+							<img class="msg-avatar" src="assets/demo/avatar/avatar_01.png" alt="avatar" />
+							<div class="msg-content">
+								<span class="name">Roxann Hollingworth <i class="fa fa-paperclip attachment"></i></span>
+								<span class="msg">Lorem ipsum dolor sit amet consectetur adipisicing elit</span>
+							</div>
+							<span class="msg-time">5m</span>
+						</a>
+					</li>
+					<li>
+						<a href="#">
+							<img class="msg-avatar" src="assets/demo/avatar/avatar_05.png" alt="avatar" />
+							<div class="msg-content">
+								<span class="name">Diamond Harlands</span>
+								<span class="msg">:)</span>
+							</div>
+							<span class="msg-time">3h</span>
+						</a>
+					</li>
+					<li>
+						<a href="#">
+							<img class="msg-avatar" src="assets/demo/avatar/avatar_02.png" alt="avatar" />
+							<div class="msg-content">
+								<span class="name">Michael Serio <i class="fa fa-paperclip attachment"></i></span>
+								<span class="msg">Sed distinctio dolores fuga molestiae modi?</span>
+							</div>
+							<span class="msg-time">12h</span>
+						</a>
+					</li>
+					<li>
+						<a href="#">
+							<img class="msg-avatar" src="assets/demo/avatar/avatar_03.png" alt="avatar" />
+							<div class="msg-content">
+								<span class="name">Matt Jones</span>
+								<span class="msg">Temporibus autem quibusdam et aut officiis debitis aut rerum necessitatibus saepe eveniet ut et voluptates repudiandae sint et mole</span>
+							</div>
+							<span class="msg-time">2d</span>
+						</a>
+					</li>
+					<li>
+						<a href="#">
+							<img class="msg-avatar" src="assets/demo/avatar/avatar_07.png" alt="avatar" />
+							<div class="msg-content">
+								<span class="name">John Doe</span>
+								<span class="msg">Neque porro quisquam est qui dolorem</span>
+							</div>
+							<span class="msg-time">7d</span>
+						</a>
+					</li>					
+				</ul>
+				<div class="dd-footer"><a href="#">View all messages</a></div>
+			</div>
+		</li>
+
+		<li class="dropdown toolbar-icon-bg demo-search-hidden mr5">
+			<a href="#" class="dropdown-toggle tooltips" data-toggle="dropdown"><span class="icon-bg"><i class="fa fa-fw fa-search"></i></span></a>
+
+			<div class="dropdown-menu arrow search dropdown-menu-form">
+				<div class="dd-header">
+					<span>Search</span>
+					<span><a href="#">Advanced search</a></span>
+				</div>
+				<div class="input-group">
+					<input type="text" class="form-control" placeholder="">
+
+					<span class="input-group-btn">
+
+						<a class="btn btn-primary" href="#">Search</a>
+					</span>
+				</div>
+			</div>
+		</li>
+
+		<li class="dropdown">
+			<a href="#" class="dropdown-toggle username" data-toggle="dropdown">
+				<span class="hidden-xs">Alexander Smith</span>
+				<img class="img-circle" src="assets/demo/avatar/avatar_06.png" alt="Dangerfield" />
+
+			</a>
+			<ul class="dropdown-menu userinfo">
+				<li><a href="#"><span class="pull-left">Edit Profile</span> <i class="pull-right fa fa-pencil"></i></a></li>
+				<li><a href="#"><span class="pull-left">Account Settings</span> <i class="pull-right fa fa-cogs"></i></a></li>
+				<li><a href="#"><span class="pull-left">Help</span> <i class="pull-right fa fa-question-circle"></i></a></li>
+				<li class="divider"></li>
+				<li><a href="#"><span class="pull-left">Earnings</span> <i class="pull-right fa fa-money"></i></a></li>
+				<li><a href="#"><span class="pull-left">Statement</span> <i class="pull-right fa fa-file-o"></i></a></li>
+				<li><a href="#"><span class="pull-left">Withdrawals</span> <i class="pull-right fa fa-credit-card"></i></a></li>
+				<li class="divider"></li>
+				<li><a href="#"><span class="pull-left">Sign Out</span> <i class="pull-right fa fa-sign-out"></i></a></li>
+			</ul>
+		</li>
+	</ul>
+</header>
---
+++ resources/views/admin/partials/master.blade.php
@@ -0,0 +1,112 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<!-- Mirrored from avalon.redteamux.com/ by HTTrack Website Copier/3.x [XR&CO'2014], Tue, 28 Oct 2014 07:38:22 GMT -->
+<head>
+	<meta charset="utf-8">
+	<title>Avalon Admin Theme</title>
+	<meta http-equiv="X-UA-Compatible" content="IE=edge,chrome=1">
+	<meta name="viewport" content="width=device-width, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0, user-scalable=no">
+	<meta name="apple-mobile-web-app-capable" content="yes">
+	<meta name="apple-touch-fullscreen" content="yes">
+	<meta name="description" content="Avalon Admin Theme">
+	<meta name="author" content="The Red Team">
+	<link href='http://fonts.googleapis.com/css?family=Source+Sans+Pro:300,400,400italic,700' rel='stylesheet' type='text/css'>
+    <link href="assets/fonts/font-awesome/css/font-awesome.min.css" type="text/css" rel="stylesheet">
+    <link href="assets/css/styles.css" type="text/css" rel="stylesheet">
+    <link href="assets/plugins/jstree/dist/themes/avalon/style.min.css" type="text/css" rel="stylesheet">
+    <link href="assets/plugins/codeprettifier/prettify.css" type="text/css" rel="stylesheet">
+    <link href="assets/plugins/iCheck/skins/minimal/blue.css" type="text/css" rel="stylesheet">
+    <link href="assets/plugins/form-daterangepicker/daterangepicker-bs3.css" type="text/css" rel="stylesheet">
+    <link href="assets/plugins/switchery/switchery.css" type="text/css" rel="stylesheet"> 
+    <link href="assets/plugins/fullcalendar/fullcalendar.css" type="text/css" rel="stylesheet">-->
+</head>
+
+    <body class="infobar-offcanvas">
+    	<script>
+    		(function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){
+    			(i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),
+    			m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
+    		})(window,document,'script','../www.google-analytics.com/analytics.js','ga');
+
+    		ga('create', 'UA-44426473-3', 'auto');
+    		ga('send', 'pageview');
+    	</script>
+
+    	@include('admin.partials.header')
+		@include('admin.partials.side-bar')
+		@yield('content')
+		<!-- @include('admin.partials.footer') -->
+
+
+
+</div>
+</div>
+</div>
+
+@include('admin.partials.side-left')
+
+
+@include('admin.partials.switch')
+<!-- Load site level scripts -->
+
+<script src="assets/js/jquery-1.10.2.min.js"></script> 							<!-- Load jQuery -->
+<script src="assets/js/jqueryui-1.9.2.min.js"></script> 							<!-- Load jQueryUI -->
+<script src="assets/js/bootstrap.min.js"></script> 								<!-- Load Bootstrap -->
+
+<script src="assets/plugins/jquery-slimscroll/jquery.slimscroll.js"></script> 	<!-- Slimscroll for custom scrolls -->
+<script src="assets/plugins/sparklines/jquery.sparklines.min.js"></script>  		<!-- Sparkline -->
+<script src="assets/plugins/jstree/dist/jstree.min.js"></script>  				<!-- jsTree -->
+
+<script src="assets/plugins/codeprettifier/prettify.js"></script> 				<!-- Code Prettifier  -->
+<script src="assets/plugins/bootstrap-switch/bootstrap-switch.js"></script> 		<!-- Swith/Toggle Button -->
+
+<script src="assets/plugins/bootstrap-tabdrop/js/bootstrap-tabdrop.js"></script>  <!-- Bootstrap Tabdrop -->
+
+<script src="assets/plugins/iCheck/icheck.min.js"></script>     					<!-- iCheck -->
+
+<script src="assets/js/enquire.min.js"></script> 										<!-- Enquire for Responsiveness -->
+
+<script src="assets/plugins/bootbox/bootbox.js"></script>					<!-- BOOTBOX -->
+
+<script src="assets/js/application.js"></script>
+<script src="assets/demo/demo.js"></script>
+<script src="assets/demo/demo-switcher.js"></script>
+
+<script src="assets/plugins/simpleWeather/jquery.simpleWeather.min.js"></script> 
+
+<!-- End loading site level scripts -->
+
+<!-- Load page level scripts-->
+
+<script src="assets/plugins/form-daterangepicker/daterangepicker.js"></script>     	<!-- Date Range Picker -->
+<script src="assets/plugins/form-daterangepicker/moment.min.js"></script>             <!-- Moment.js for Date Range Picker -->
+
+<script src="assets/plugins/easypiechart/jquery.easypiechart.js"></script> 			<!-- EasyPieChart -->
+<script src="assets/plugins/powerwidgets/js/powerwidgets.js"></script> 				<!-- PowerWidgets -->
+<script src="assets/plugins/switchery/switchery.js"></script>     					<!-- Switchery -->
+
+<script src="assets/plugins/fullcalendar/fullcalendar.min.js"></script>   			<!-- FullCalendar -->
+
+<!-- Charts -->
+<script src="assets/plugins/charts-flot/jquery.flot.min.js"></script>             	<!-- Flot Main File -->
+<script src="assets/plugins/charts-flot/jquery.flot.stack.min.js"></script>       	<!-- Flot Stacked Charts Plugin -->
+<script src="assets/plugins/charts-flot/jquery.flot.orderBars.min.js"></script>   	<!-- Flot Ordered Bars Plugin-->
+<script src="assets/plugins/charts-flot/jquery.flot.resize.min.js"></script>          <!-- Flot Responsive -->
+<script src="assets/plugins/charts-flot/jquery.flot.tooltip.min.js"></script> 		<!-- Flot Tooltips -->
+
+<!-- Maps -->
+<script src="assets/plugins/jQuery-Mapael/js/raphael/raphael-min.js"></script>        <!-- Load Raphael as Dependency -->
+<script src="assets/plugins/jQuery-Mapael/js/jquery.mapael.js"></script>              <!-- jQuery Mapael -->
+<!-- <script src="assets/plugins/jQuery-mousewheel/jquery.mousewheel.min.js"></script> -->     <!-- Mousewheel Support in zoomed-in maps -->
+<script src="assets/plugins/jQuery-Mapael/js/maps/world_countries.js"></script>       <!-- Vector Data of World Countries -->
+
+
+<script src="assets/demo/demo-index.js"></script> 									<!-- Initialize scripts for this page-->
+
+<!-- End loading page level scripts-->
+
+</body>
+
+<!-- Mirrored from avalon.redteamux.com/ by HTTrack Website Copier/3.x [XR&CO'2014], Tue, 28 Oct 2014 07:42:50 GMT -->
+</html>
